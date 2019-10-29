5b6c463fa95d97b2d9a47ff0d2489932a11f066b
lrc035 <37704259+lrc035@users.noreply.github.com>
2019-10-29 15:51:37 -0500

    Update lab4_2f.py

--- lab4_2f.py
+++ lab4_2f.py
@@ -3,8 +3,21 @@ Lab 4: Q2f: Retrieve a bucket policy
 """
 
 import boto3
+import json
 
 s3_client = boto3.client('s3')
 bucket_name = 'lrc035lab42abuc'
+bucket_policy = {
+    'Version' : '2012-10-17',
+    'Statement' : [{
+        'Sid' : 'AddPerm',
+        'Effect' : 'Allow',
+        'Principal' : '*',
+        'Action' : ['s3:GetObject'],
+        'Resource' : "arn:aws:s3:::%s/*" % bucket_name
+    }]
+}
+bucket_policy = json.dumps(bucket_policy)
+s3_client.put_bucket_policy(Bucket=bucket_name,Policy=bucket_policy)
 response = s3_client.get_bucket_policy(Bucket=bucket_name)
 print(response['Policy'])
