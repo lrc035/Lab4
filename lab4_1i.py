2790a207cae7a7aa009d12b97d5d28f696462522
lrc035 <37704259+lrc035@users.noreply.github.com>
2019-10-29 14:48:00 -0500

    Update lab4_1i.py

--- lab4_1i.py
+++ lab4_1i.py
@@ -8,7 +8,7 @@ from botocore.exceptions import ClientError
 ec2 = boto3.client('ec2')
 
 try:
-    response = ec2.create_security_groups(GroupName='Lab4_1i', Description='Lab4_1i created 10/29/19')
+    response = ec2.create_security_group(GroupName='Lab4_1i', Description='Lab4_1i created 10/29/19')
     print("Success. ", response)
 except ClientError as e:
     print(e)
